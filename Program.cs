ffb54d4b4bb8a2675da5fdb290c294568f5bb40d
Luca <lucabarcontatos@gmail.com>
2026-09-09 19:23:09 -0300

    consumo do api via cep

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json;
+using ConsumerViaCep.Models;
+using static System.Console;
+
+WriteLine("Digite o seu CEP: ");
+var cep = ReadLine();
+
+var enderecoUrl = $@"https://viacep.com.br/ws/{cep}/json/";
+
+WriteLine($"Realizando requisisição para o endpoint: {enderecoUrl}");
+
+var client = new HttpClient();
+
+try
+{
+    HttpResponseMessage? response = await client.GetAsync(enderecoUrl);
+    response.EnsureSuccessStatusCode();
+
+    string responseString = await response.Content.ReadAsStringAsync();
+
+    Endereco? enderecoRetornadoDaApi = JsonSerializer.Deserialize<Endereco>(responseString);
+    
+WriteLine($"CEP: {enderecoRetornadoDaApi?.Cep}");
+WriteLine($"Rua: {enderecoRetornadoDaApi?.Logradouro}");
+WriteLine($"Cidade: {enderecoRetornadoDaApi?.Localidade}");}
+catch (System.Exception e)
+{
+    WriteLine("Erro: " + e.InnerException);
+    WriteLine("Aconteceu um erro ao consultar a api: " + e.Message);
+}
